b028af90e0f1c4f9c5288c3cce019f78967e69da
Genival Jr <moreno.alado@gmail.com>
2012-10-18 08:58:37 -0300

    Quem Somos, Servicos/Produtos e FB Inclusos

--- index.php
+++ index.php
@@ -1,19 +1,38 @@
 <?php include("header.php"); ?>
 	<title><?php echo $core_nome;?>| Home</title>
 	</head>
-		<body>
-			<div id="header">
-				<div id="topo" class="centraliza">
-        			<?php include ("topo.php"); ?>        	
-				</div>
+	<body>
+		<div id="header">
+			<div id="topo" class="centraliza">
+				<?php include ("topo.php"); ?>        	
 			</div>
-      
-<div id="content"> </div>
-
-	<div id="navigation"> </div>
-	</div>
-	<div id="footer">
-    	<?php include ("footer.php"); ?>
-    	
-</body>
+		</div>
+		<div id="content">
+        	<div id="home" class="centraliza corpo">
+            	<div id="slider_area">
+            		<div id="slider">
+                    	<img src="<?php servidor(); ?>images/slide_home/1.jpg" alt="">
+                        <img src="<?php servidor(); ?>images/slide_home/2.jpg" alt="">
+                    </div>
+                    <div class="coluna">
+                    	<div id="conheca">
+                        	<a href=""><img src="<?php servidor(); ?>images/home/quemsomos_bt.png" alt=""></a>
+                        </div>
+                        <div id="servprod">
+                        	<a href=""><img src="<?php servidor(); ?>images/home/servprod_bt.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="coluna">
+                    	<div id="face"></div>
+                    </div>
+                    <div class="coluna"></div>
+                    <div class="separator"></div>
+            	</div>
+            </div>
+        
+        </div>
+		<div id="footer">
+			<?php include ("footer.php"); ?>
+		</div>
+	</body>
 </html>
--- topo.php
+++ topo.php
@@ -1,21 +1,21 @@
- 		<div class="centraliza">
-        	<img src="<?php servidor();?>images/logo.png" id="logo">            
-            <div id="redes">
-            	<img src="images/contato.png" id="cont">
-            	<img src="images/face.png" id="rede-fac">
-                <img src="images/twiter.png" id="rede-twit">
-            </div>
-        </div>
+					
+                    <div class="centraliza">
+						<img src="<?php servidor();?>images/logo.png" id="logo">            
+            			<div id="redes">
+            				<img src="images/contato.png" id="cont">
+            				<img src="images/face.png" id="rede-fac">
+                			<img src="images/twiter.png" id="rede-twit">
+            			</div>
+        			</div>
         
-        <div class="separator"></div>
-        <div id="navigation">
-	        <ul>
-		        <li><a href=""><img src="images/menu_principal/home_off.png" alt="" class="botao current"></a></li>
-		        <li><a href=""><img src="images/menu_principal/quem-somos_off.png" alt="" class="botao"></a></li>
-		        <li><a href=""><img src="images/menu_principal/servicos_off.png" alt=""class="botao"></a></li>
-		        <li><a href=""><img src="images/menu_principal/engenharia_off.png" alt="" class="botao"></a></li>
-		        <li><a href=""><img src="images/menu_principal/localizacao_off.png" alt="" class="botao"></a></li>
-		        <li><a href=""><img src="images/menu_principal/contato_off.png" alt="" class="botao"></a></li>
-	        </ul>
-        
-        </div>
+        			<div class="separator"></div>
+			        <div id="navigation">
+				        <ul class="centraliza">
+					        <li><a href=""><img src="images/menu_principal/home_off.png" alt="" class="botao current"></a></li>
+					        <li><a href=""><img src="images/menu_principal/quem-somos_off.png" alt="" class="botao"></a></li>
+					        <li><a href=""><img src="images/menu_principal/servicos_off.png" alt=""class="botao"></a></li>
+					        <li><a href=""><img src="images/menu_principal/engenharia_off.png" alt="" class="botao"></a></li>
+					        <li><a href=""><img src="images/menu_principal/localizacao_off.png" alt="" class="botao"></a></li>
+					        <li><a href=""><img src="images/menu_principal/contato_off.png" alt="" class="botao"></a></li>
+				        </ul>
+			        </div>	
