3feba3aa8a917cbe940ac15bb7d9e31beeacf2dd
edevaldobatista <ed_batista.junior@hotmail.com>
2012-10-18 11:11:52 -0300

    facebook adicionado

--- index.php
+++ index.php
@@ -23,7 +23,11 @@
                         </div>
                     </div>
                     <div class="coluna">
-                    	<div id="face"></div>
+                    	<div id="face">
+							<script type="text/javascript" src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php/en_US"></script>
+					<script type="text/javascript">FB.init("");</script>
+                    <fb:fan profile_id="541734165841034" stream="0" connections="5" width="300px" height="160px" header="0" logobar="0"   css=http://talentodesign.com.br/stylesheets/fb_lpx.css?2"></fb:fan>
+                      </div>
                     </div>
                     <div class="coluna"></div>
                     <div class="separator"></div>
