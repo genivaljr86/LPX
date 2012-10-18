f1c225e482547f513ce14a69ad1cc3c01f94db6f
Genival Jr <moreno.alado@gmail.com>
2012-10-18 15:49:33 -0300

    Concluido Layout da Homepage

--- index.php
+++ index.php
@@ -14,24 +14,36 @@
                     	<img src="<?php servidor(); ?>images/slide_home/1.jpg" alt="">
                         <img src="<?php servidor(); ?>images/slide_home/2.jpg" alt="">
                     </div>
-                    <div class="coluna">
-                    	<div id="conheca">
-                        	<a href=""><img src="<?php servidor(); ?>images/home/quemsomos_bt.png" alt=""></a>
-                        </div>
-                        <div id="servprod">
-                        	<a href=""><img src="<?php servidor(); ?>images/home/servprod_bt.png" alt=""></a>
-                        </div>
+				</div>
+                <div class="separator"></div>
+                <div class="coluna">
+                	<div id="conheca">
+                    	<a href=""><img src="<?php servidor(); ?>images/home/quemsomos_bt.png" alt=""></a>
                     </div>
-                    <div class="coluna">
-                    	<div id="face">
-							<script type="text/javascript" src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php/en_US"></script>
-					<script type="text/javascript">FB.init("");</script>
-                    <fb:fan profile_id="541734165841034" stream="0" connections="5" width="300px" height="160px" header="0" logobar="0"   css=http://talentodesign.com.br/stylesheets/fb_lpx.css?2"></fb:fan>
-                      </div>
+                    <div id="servprod">
+                       	<a href=""><img src="<?php servidor(); ?>images/home/servprod_bt.png" alt=""></a>
                     </div>
-                    <div class="coluna"></div>
-                    <div class="separator"></div>
+                </div>
+                <div class="coluna">
+                   	<div id="face">
+						<script type="text/javascript" src="http://static.ak.connect.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php/en_US"></script>
+				    	<script type="text/javascript">FB.init("");</script>
+                        <fb:fan profile_id="541734165841034" stream="0" connections="5" width="300px" height="160px" header="0" logobar="0"   css="http://talentodesign.com.br/stylesheets/fb_lpx.css?2"></fb:fan>
+                    </div>
+                    <div id="falecon">
+                    	<a href=""><img src="<?php servidor(); ?>images/home/falecon_bt.png" alt=""></a>
+                    </div>
             	</div>
+                <div class="coluna c3">
+                	<div id="local">
+                    	<a href=""><img src="<?php servidor(); ?>images/home/local_bt.png" alt=""></a>
+                    </div>
+                    <div id="twt">
+                    	<div id="tt"></div>
+                        <a href=""><img src="<?php servidor(); ?>images/home/twt_bt.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="separator"></div>
             </div>
         
         </div>
